26f066f702e30b8370a3048902e0cba9e2b8381a
usamah abdul matin <eexvuu@gmail.com>
2023-10-15 14:00:34 +0700

    fixing test

---
+++ next-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+/// <reference types="react-scripts" />
+declare module '*.mp3'; // '*.wav' if you're using wav format
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
