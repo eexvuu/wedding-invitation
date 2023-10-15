68d1c297dfabb38f40a1b3d4728358d2b613c1b5
usamah abdul matin <eexvuu@gmail.com>
2023-10-15 13:46:11 +0700

    adding music

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -1,10 +1,17 @@
-
+"use client";
+import music from "../assets/backsound.mp3";
 function Music() {
+  const playMusic = () => {
+    new Audio(music).play();
+  };
   return (
     <>
-      <div className="bg-red-500 text-white fixed right-5 bottom-5 p-5 rounded-full cursor-pointer">
+      <button
+        className="bg-red-500 text-white fixed right-5 bottom-5 p-5 rounded-full cursor-pointer"
+        onClick={playMusic}
+      >
         music
-      </div>
+      </button>
     </>
   );
 }
