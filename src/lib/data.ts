f71d2a79ce276f9ac4fb93978963c3b2f0d04614
usamah <eexvuu@gmail.com>
2023-12-01 21:43:46 +0700

    test update data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -4,9 +4,9 @@ import background from "@/assets/bg.jpg";
 // Define the return type of the getData function
 const getData = (): { data: WeddingData } => ({
   data: {
-    namaMempelaiPria: "Andi Hermansyah",
-    namaMempelaiWanita: "Aisyah Salsabila",
-    tanggalPernikahan: "01 Januari 2022",
+    namaMempelaiPria: "Usamah Abdul Matin",
+    namaMempelaiWanita: "Kim Min-ji",
+    tanggalPernikahan: "01 Januari 2024",
     tempatPernikahan: "Jakarta Selatan, Indonesia",
     background: background,
   },
